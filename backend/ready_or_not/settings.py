95d3be2cd2fcd867b3a8ee2720c2bb0da703e526
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-07-25 17:28:54 -0600

    Add google to authorized authentications

--- backend/ready_or_not/settings.py
+++ backend/ready_or_not/settings.py
@@ -65,10 +65,19 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = (
+    'social_core.backends.open_id.OpenIdAuth',
+    'social_core.backends.google.GoogleOpenId',
+    'social_core.backends.google.GoogleOAuth2',
+    'django.contrib.auth.backends.ModelBackend',
+)
 
 WSGI_APPLICATION = 'ready_or_not.wsgi.application'
 
