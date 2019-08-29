f996dc226e085414bcabd10e272bb225187c365e
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-08-28 23:55:10 -0600

    Adds spec folder, User model spec

---
+++ backend/spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe User, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :uid }
+    it { should validate_presence_of :username }
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
+  end
+
+  describe 'relationships' do
+  end
+
+end
