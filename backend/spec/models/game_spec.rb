9db11351ffccaffca5ac08b5f19eaf4d0de0eb7d
Peregrine Balas <peregrinereedbalas@gmail.com>
2023-10-30 23:34:59 -0500

    update tests

--- backend/spec/models/game_spec.rb
+++ backend/spec/models/game_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
-describe Round, type: :model do
+describe Game, type: :model do
     describe 'validations' do
-        it { should validate_presence_of :timer }
+        it { should validate_presence_of :round_count }
         it { should validate_presence_of :active }
+        it { should validate_presence_of :complete }
     end
 
     describe 'relationships' do
-        it { should belong_to :game }
+        it { should belong_to :user }
+        it { should have_many :rounds }
     end  
 
     describe 'instance methods' do
