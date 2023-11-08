032fd798b26a85e9a3efb1c962c9e0dae92e4c93
Peregrine Balas <peregrinereedbalas@gmail.com>
2023-11-08 17:04:46 -0600

    add field for player that is it

--- backend/db/migrate/20231101235342_create_players.rb
+++ backend/db/migrate/20231101235342_create_players.rb
@@ -4,6 +4,7 @@ class CreatePlayers < ActiveRecord::Migration[6.1]
       t.references :user, null: false, foreign_key: true
       t.references :round, null: false, foreign_key: true
       t.boolean :winner
+      t.boolean :it
 
       t.timestamps
     end
