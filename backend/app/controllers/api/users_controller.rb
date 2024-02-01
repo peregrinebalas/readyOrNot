ddf90f36d463762a9dc91621f2e71a4ba8c7aba2
Peregrine Balas <peregrinereedbalas@gmail.com>
2024-01-31 22:00:23 -0600

    controller updates

--- backend/app/controllers/api/users_controller.rb
+++ backend/app/controllers/api/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+    def create
+        User.new()
+    end
+
+    def destroy
+        
+    end
+
+    def update
+
+    end
+
+    def show
+
+    end
+
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
 end
